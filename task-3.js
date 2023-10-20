4380b06fcc60b397db0e1ade0bc8d92f33f04465
gluxenka <gluxenka750@gmail.com>
2023-10-20 16:25:27 +0300

    finish hm2

---
+++ task-3.js
@@ -0,0 +1,19 @@
+
+/**
+ * 
+ * @param {string} message 
+ */
+function checkForSpam(message) {
+    const preparedMsg = message.toLowerCase();
+    const containsWords = preparedMsg.includes('spam') || preparedMsg.includes('sale');
+    return containsWords;
+}
+
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
